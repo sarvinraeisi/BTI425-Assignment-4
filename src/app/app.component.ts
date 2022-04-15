7f5b957c8eb8bc469c681305577cd94c23573fc4
sarvin <sraeisi2@myseneca.ca>
2022-04-15 11:58:28 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 /*********************************************************************************
-* BTI425 – Assignment 03
+* BTI425 – Assignment 04
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or
 * distributed to other students.
 *
-* Name: ___sarvin raeisi____ Student ID: __163927197__ Date: _26/03/2022____
+* Name: ___sarvin raeisi____ Student ID: __163927197__ Date: _april 15 2022____
 *
 ********************************************************************************/
 import { Component } from '@angular/core';
